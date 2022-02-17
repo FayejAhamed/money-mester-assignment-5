575f8b50633faf0aa8fcf448c81b9908f9fff4e6
fayejahamed <foyezfoez017@gmail.com>
2022-02-17 00:34:29 -0500

    second commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
-
-
+function expensesInput() {
+    const foodInputText = document.getElementById('food-input').value;
+    const foodInput = parseFloat(foodInputText);
+    const rentInputText = document.getElementById('rent-input').value;
+    const rentInput = parseFloat(rentInputText);
+    const clothesInputText = document.getElementById('clothes-input').value;
+    const clothesInput = parseFloat(clothesInputText);
+    // expeneses adding
+    const totalInputExpenses = foodInput + rentInput + clothesInput;
+    const totalExpenses = document.getElementById('total-expenses').innerText = totalInputExpenses;
+    // total amount calcalution
+    const incomeInputText = document.getElementById('income-input').value;
+    const incomeInput = parseFloat(incomeInputText);
+    const totalBalance = document.getElementById('total-balance').innerText = incomeInput - totalExpenses;
+    // console.log(totalBalance.innerText)
+}
+// calculate button 
 document.getElementById('calculate-button').addEventListener('click', function () {
-    console.log('clicked')
+    // console.log(iputFeild)
+    const Expenses = expensesInput();
+    // console.log(foodExpenses)
+
 })
+
+// save button 
