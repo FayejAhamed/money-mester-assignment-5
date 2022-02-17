6bce346387c8f2b6b803c579fcf0fd6498abd9d1
fayejahamed <foyezfoez017@gmail.com>
2022-02-17 06:09:01 -0500

    second commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+
 function expensesInput() {
     const foodInputText = document.getElementById('food-input').value;
     const foodInput = parseFloat(foodInputText);
@@ -6,38 +8,51 @@ function expensesInput() {
     const clothesInputText = document.getElementById('clothes-input').value;
     const clothesInput = parseFloat(clothesInputText);
     // expeneses adding
+    // if ()
     const totalInputExpenses = foodInput + rentInput + clothesInput;
     const totalExpenses = document.getElementById('total-expenses').innerText = totalInputExpenses;
-    // total amount calcalution
     const incomeInputText = document.getElementById('income-input').value;
     const incomeInput = parseFloat(incomeInputText);
-    const totalBalance = document.getElementById('total-balance').innerText = incomeInput - totalExpenses;
-    // console.log(totalBalance.innerText)
+    if (totalInputExpenses > incomeInput) {
+        alert("You can't spend more mony than you have, try again")
+    }
+    else if (totalInputExpenses < 0) {
+        alert('Plese provide a positive number')
+    }
+    return totalExpenses;
+}
+function income() {
+    const incomeInputText = document.getElementById('income-input').value;
+    const incomeInput = parseFloat(incomeInputText);
+    const totalBalance = document.getElementById('total-balance').innerText = incomeInput - expensesInput();
 }
 
 // save function 
 function saveAmount() {
+    // saving money
     const saveInputText = document.getElementById('save-input');
     const saveInput = parseFloat(saveInputText.value);
     const incomeInputText = document.getElementById('income-input');
     const incomeInput = parseFloat(incomeInputText.value);
     const savingAmount = document.getElementById('save-amount');
-    const totalSavingAmount = incomeInput / saveInput;
-    console.log(totalSavingAmount)
-
+    const totalSavingAmount = (incomeInput * saveInput) / 100;
     savingAmount.innerText = totalSavingAmount;
-
+    // remaining balance after uses 
+    const totalBalanceText = document.getElementById('total-balance');
+    const totalBalance = parseFloat(totalBalanceText.innerText);
+    const remainingBalance = document.getElementById('remaining-balance').innerText = totalBalance - totalSavingAmount;
+    // error message 
+    if (remainingBalance > 0) {
+        alert('You have very little money')
+    }
 }
-
-
 // calculate button 
 document.getElementById('calculate-button').addEventListener('click', function () {
     // console.log(iputFeild)
     const Expenses = expensesInput();
-    // console.log(foodExpenses)
+    income();
 
 })
-
 // save button 
 document.getElementById('save-button').addEventListener('click', function () {
     console.log('clicked save button')
